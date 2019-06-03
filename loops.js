d68e716928d75ff5ae4eb83da186088303b1bb7f
Monica Lee <mylee.715@gmail.com>
2019-06-03 15:36:28 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am 1 strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+return array;
+}
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n);
+  }
+return 'done';
+}
+
+
+function doWhileLoop(i) {
+  do {
+    console.log("I run once regardless.");
+    i++;
+  } while (-1 < incrementalVariable() < 10);
+}
